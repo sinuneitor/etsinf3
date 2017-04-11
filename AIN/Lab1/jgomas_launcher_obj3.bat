22d7c05d57b50a7821c49e36d225283d259ec65b
Carlos Galindo <carlos.s.galindo@gmail.com>
2017-04-11 13:18:37 +0200

    AIN Lab1 completed

---
+++ AIN/Lab1/jgomas_launcher_obj3.bat
@@ -0,0 +1,1 @@
+java -classpath "lib\jade.jar;lib\jadeTools.jar;lib\Base64.jar;lib\http.jar;lib\iiop.jar;lib\beangenerator.jar;lib\jgomas.jar;lib\jason.jar;lib\JasonJGomas.jar;classes;." jade.Boot -container -host localhost "A2:es.upv.dsic.gti_ia.JasonJGomas.BasicTroopJasonArch(jasonAgent_ALLIED_obj3.asl)"
