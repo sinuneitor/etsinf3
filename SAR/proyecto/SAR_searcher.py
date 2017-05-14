9501c8f671171792b6964457a6bdcc1a393072cc
Carlos Galindo <carlos.s.galindo@gmail.com>
2017-05-14 14:23:21 +0200

    SAR proyecto searcher correct output on search

--- SAR/proyecto/SAR_searcher.py
+++ SAR/proyecto/SAR_searcher.py
@@ -1,5 +1,6 @@
 import pickle
 import sys
+import re
 
 def processQuery(index, query):
     pLists = []
@@ -30,6 +31,22 @@ def processQuery(index, query):
                 y += 1
     return res
 
+def procesarNoticia(texto):
+    texto = "".join([c if c.isalpha() else " " for c in texto])
+    return [w.lower() for w in re.split(delimiter_word, texto)]
+
+def snippet(text, wordlist):
+    words = procesarNoticia(text);
+    #words = re.split(delimiter_word,text)
+    snippet = ""
+    for i in range(0,len(words)):
+        word = words[i]
+        if word in wordlist:
+            snippet = snippet + "..."
+            for j in range(max(0,i-3),min(len(words),i+4)):
+                snippet = snippet + words[j] + " "
+            snippet = snippet + "...\n"
+    return snippet
 
 # Process arguments
 if len(sys.argv) != 2:
@@ -41,6 +58,12 @@ index_file = sys.argv[1]
 with open(index_file, "rb") as f:
     (index, docIndex) = pickle.load(f)
 
+
+delimiter_noticia = re.compile("<DOC>")
+delimiter_text = re.compile("</?TEXT>")
+delimiter_title = re.compile("</?TITLE>")
+delimiter_word = re.compile("[\n\t ]")
+
 # Infinite query loop (end with '')
 print("TIP: you can write !! to insert your previous query")
 prev = ""
@@ -49,7 +72,30 @@ while query != '':
     query = query.replace("!!", prev)
     prev = query
     wordlist = query.lower().split()
-    print(len(processQuery(index, wordlist)))
+    res = processQuery(index, wordlist)
+    dant = -1
+
+    cont = 0
+    for (d,p) in res:
+        if(cont>9):
+            continue
+        if dant != d:
+            data = open(docIndex.get(d)).read()
+            dant = d
+        news_list = re.split(delimiter_noticia, data)
+        new = news_list[p+1]
+        title = re.split(delimiter_title,new)
+        if len (title)>1:
+            print(title[1])
+        if len(res)<3:
+            text = re.split(delimiter_text,new)
+            if len (title)>1:
+                print(text[1])
+        elif len(res)<6:
+            text = re.split(delimiter_text,new)
+            print(snippet(text[1], wordlist))
+        cont+=1
+
     query = input("Your query > ")
 
 print("The program will now end.")
